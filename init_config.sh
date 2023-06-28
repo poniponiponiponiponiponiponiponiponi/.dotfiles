5f59257a4b871fea48a276175e755d637cdce6c3
tabun dareka <tabun.dareka@protonmail.com>
2023-06-28 16:50:01 +0200

    bugfixes

--- init_config.sh
+++ init_config.sh
@@ -5,6 +5,8 @@ DOTFILES_PATH="$HOME/.dotfiles"
 mkdir -p ~/.config
 mkdir -p ~/.config/mpv
 mkdir -p ~/.config/tmux
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/helix
 
 ln -sf "$DOTFILES_PATH/mpv.conf" ~/.config/mpv/mpv.conf
 ln -sf "$DOTFILES_PATH/pwninit_template.py" ~/.config/pwninit_template.py
@@ -13,8 +15,8 @@ ln -sf "$DOTFILES_PATH/alacritty.yml" ~/.config/alacritty.yml
 ln -sf "$DOTFILES_PATH/zshrc" ~/.zshrc
 ln -sf "$DOTFILES_PATH/nvim-config" ~/.config/nvim
 ln -sf "$DOTFILES_PATH/sway_config" ~/.config/i3/config
-ln -sf "$DOTFILES_PATH/language.toml" ~/.config/i3/config/helix/
-ln -sf "$DOTFILES_PATH/config.toml" ~/.config/i3/config/helix/
+ln -sf "$DOTFILES_PATH/language.toml" ~/.config/helix/language.toml
+ln -sf "$DOTFILES_PATH/config.toml" ~/.config/helix/config.toml
 
 if command -v ipython &> /dev/null
 then
@@ -23,4 +25,11 @@ then
     ln -sf $DOTFILES_PATH/ctf_ipython_config.py ~/.config/ipython/profile_ctf/ipython_config.py
 else
     echo "NO IPYTHON DETECTED"
+fi
+
+if command -v git &> /dev/null
+then
+    git config --global pager.branch false
+else
+    echo "NO GIT DETECTED"
 fi
