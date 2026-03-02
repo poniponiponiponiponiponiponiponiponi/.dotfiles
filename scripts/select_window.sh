45a8ad8faf02236606ac403648809e4cd1b9f6c2
poniponiponiponiponiponiponiponiponi <poniponiponiponiponiponiponiponiponiponi@protonmail.com>
2026-03-02 20:07:34 +0100

    dmenu font

--- scripts/select_window.sh
+++ scripts/select_window.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-selected=$(wmctrl -l | awk '{$1=$1; $2=$2; $3=$3; sub(/^[^ ]+ [^ ]+ [^ ]+ /,""); print $0}' | dmenu -i -l 20)
+selected=$(wmctrl -l | awk '{$1=$1; $2=$2; $3=$3; sub(/^[^ ]+ [^ ]+ [^ ]+ /,""); print $0}' | dmenu -i -l 20 -nb "#282828" -nf "#ebdbb2" -sb "#83a598" -sf "#282828" -fn "JetBrainsMono Nerd Font:size=12")
 win_id=$(wmctrl -l | grep -F "$selected" | awk '{print $1}')
 wmctrl -i -a "$win_id"
