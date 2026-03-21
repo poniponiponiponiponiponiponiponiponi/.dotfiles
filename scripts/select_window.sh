08c98b01bd238995ec1d3145e57235504d5b226c
poniponiponiponiponiponiponiponiponi <poniponiponiponiponiponiponiponiponiponi@protonmail.com>
2026-03-21 15:45:16 +0100

    pretty colors

--- scripts/select_window.sh
+++ scripts/select_window.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-selected=$(wmctrl -l | awk '{$1=$1; $2=$2; $3=$3; sub(/^[^ ]+ [^ ]+ [^ ]+ /,""); print $0}' | dmenu -i -l 20 -nb "#282828" -nf "#ebdbb2" -sb "#83a598" -sf "#282828" -fn "JetBrainsMono Nerd Font:size=12")
+selected=$(wmctrl -l | awk '{$1=$1; $2=$2; $3=$3; sub(/^[^ ]+ [^ ]+ [^ ]+ /,""); print $0}' | dmenu -i -l 20 -nb "#212026" -nf "#b2b2b2" -sb "#bc6ec5" -sf "#212026" -fn "JetBrainsMono Nerd Font:size=12")
 win_id=$(wmctrl -l | grep -F "$selected" | awk '{print $1}')
 wmctrl -i -a "$win_id"
